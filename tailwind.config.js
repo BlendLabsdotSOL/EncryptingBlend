2dce3bc678bbbaa84f9ef917eea7aba69ec245a4
BlendLabsdotSOL <excaliburmoney328@gmail.com>
2025-11-12 13:45:41 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6D28D9",
+        "primary-dark": "#5B21B6",
+      },
+    },
+  },
+  plugins: [],
+};
